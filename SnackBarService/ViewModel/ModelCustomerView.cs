5f8783de3d024c7d3a5add696ccad81c929b21a1
Fox160 <ksenija-kstenk@mail.ru>
2018-04-20 10:46:59 +0400

    Лаба 13 Вариант 12 C#

--- SnackBarService/ViewModel/ModelCustomerView.cs
+++ SnackBarService/ViewModel/ModelCustomerView.cs
@@ -1,9 +1,14 @@
-﻿namespace SnackBarService.ViewModel
+﻿using System.Runtime.Serialization;
+
+namespace SnackBarService.ViewModel
 {
+    [DataContract]
     public class ModelCustomerView
     {
+        [DataMember]
         public int ID { get; set; }
 
+        [DataMember]
         public string CustomerFullName { get; set; }
     }
 }
--- SnackBarService/ViewModel/ModelExecutorView.cs
+++ SnackBarService/ViewModel/ModelExecutorView.cs
@@ -1,9 +1,14 @@
-﻿namespace SnackBarService.ViewModel
+﻿using System.Runtime.Serialization;
+
+namespace SnackBarService.ViewModel
 {
+    [DataContract]
     public class ModelExecutorView
     {
+        [DataMember]
         public int ID { get; set; }
 
+        [DataMember]
         public string ExecutorFullName { get; set; }
     }
 }
